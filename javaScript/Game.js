e343212d888c82cc0402d2d02f83c27eae04004d
Cristina Tabares <cristina_tabares@hotmail.es>
2023-04-21 15:03:20 +0200

    initial commit

---
+++ javaScript/Game.js
@@ -0,0 +1,28 @@
+class Game{
+    constructor(){
+        this.background = new Image()
+        this.background.src = "imagenes/fondoGalaxia.png"
+        this.nave = new Nave()
+
+    }
+
+    drawBackground = ()=>{
+        ctx.drawImage(this.background, 0, 0, canvas.width, canvas.height)
+    }
+
+    gameLoop = ()=>{
+        // 1 limpieza del canvas
+
+        // 2. acciones y movimientos de los elementos
+
+
+        // 3 dibujado de los elementos
+        this.drawBackground()
+        this.nave.draw()
+
+        // 4. recursion
+
+        requestAnimationFrame(this.gameLoop)
+
+    }
+}
--- javaScript/main.js
+++ javaScript/main.js
@@ -1,5 +1,31 @@
 //* VARIABLES GLOBALES
-
+const splasScreenDOM = document.querySelector("#pantalla-principal");
+const gameoverScreenDOM = document.querySelector("#pantalla-gameover");
+const startBtnDOM = document.querySelector("#start-btn");
+const restartBtnDOM = document.querySelector("#restart-btn");
+const canvas = document.querySelector("#my-canvas");
+const ctx = canvas.getContext("2d");
+let gameObj;
 
 
 //* FUNCIONES DE ESTADO
+
+const startGame = ()=>{
+    console.log("Iniciando")
+    splasScreenDOM.style.display = "none";
+  canvas.style.display = "block";
+  gameObj = new Game();
+  console.log(gameObj);
+  gameObj.gameLoop()
+}
+const restartGame = ()=>{
+    gameoverScreenDOM.style.display = "none";
+    canvas.style.display = "block";
+    gameObj = new Game();
+    gameObj.gameLoop()
+}
+
+
+//* ADD EVENT LISTENERRS
+startBtnDOM.addEventListener("click", startGame);
+restartBtnDOM.addEventListener("click", restartGame)
